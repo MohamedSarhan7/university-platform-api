d29e1acac7df384df9e0340edfcadd0632474ceb
Your Name <you@example.com>
2022-07-01 02:01:49 +0200

    whitenoise

--- backend/settings.py
+++ backend/settings.py
@@ -180,7 +180,11 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 django_heroku.settings(locals())
 MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/image/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
